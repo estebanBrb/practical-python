e105c6cf417e53a8dac4be3d4f4147a742be5c3b
estebanBrb <estebanbarberan@gmil.com>
2022-11-03 20:12:52 -0300

    Agregados ejercicios 2.15, 2.16 a pcost.py

--- Work/pcost.py
+++ Work/pcost.py
@@ -27,3 +27,43 @@ else:
 # llamado desde la cl: python3 pcost.py Data/portfolio.csv
 cost = portfolio_cost(filename)
 print(f'Total cost {cost}')
+
+#
+#
+# Exercise 2.15
+
+def portfolio_cost_alt(filename):
+    with open(filename, 'rt') as f:
+        headers = next(f)
+        total_cost = 0
+        for lineno, line in enumerate(f, start = 1):
+            try:
+                row = line.split(',')
+                total_cost += int(row[1]) * float(row[2])
+            except ValueError:
+                print(f'La fila {lineno}: No tiene el formato adecuado: {row}')
+    
+    return total_cost
+
+#
+#
+# Exercise 2.16
+import csv
+
+def portfolio_cost_zip(filename):
+    with open(filename, 'rt') as f:
+        filas = csv.reader(f)
+        headers = next(filas)
+        total_cost = 0
+        for lineno, line in enumerate(filas, start = 1):
+            record = dict(zip(headers, line))
+            #print(record)
+            try:
+                nshares = int(record['shares'])
+                price = float(record['price'])
+                total_cost += nshares * price
+            except ValueError:
+                print(f'La fila {lineno}: No tiene el formato adecuado: {line}')
+    
+    return total_cost
+
